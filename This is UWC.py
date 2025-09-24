4d9f3ef12731256805e3a068e7b45f28d91d6da4
Yonas G Bekele <ybekele25@uwcchina.org>
2025-09-24 12:08:01 +0800

    I have added typecasting

---
+++ This is UWC.py
@@ -0,0 +1,38 @@
+#This is UWC Changshu China Campus 
+print ("This is UWC Changshu China Campus: The best UWC campus ever!")
+print ("Yonas Getenet Bekele")
+first_name = "Yonas"
+last_name = "Bekele"
+middle_name = "Getenet"
+full_name = first_name + middle_name + last_name
+print (full_name)
+print (f'My full name is {first_name} {middle_name} {last_name}.')
+age =16 
+
+#Strings and f-strings 
+
+food = "Doro Wat"
+print (f'My name is {first_name} {middle_name} {last_name}, I am {age} years old and my favorite food is {food}.')
+email ="yonasfake@gmail.com"
+print (f'My email is {email} and shoot me at whatever time you want!')
+
+#Integers are usually whole numbers that are unsed for counting purposes and they don't take a quotation marks as a string unless and otherwise they will be considered as strings. 
+
+print (f'You are {age} years old.')
+
+#f strings are used to format strings and they are usually used when we want to include or combine variables and strings together.
+id_number = 2394943 
+print (f'Your id number is {id_number} and use is always for identification and verification purposes.')
+height = 1.75 
+print ("What is your height in meters?")
+print (f'My height is {height} meters.')
+weight = 70
+print (f'My weight is {weight} kilograms')\
+
+
+#float are usually decimal numbers and they are used when we want to be more precise about a number. And also they will show precision in our numbers. 
+#integers don't take string quoatation marks otherwise they will be considered as strings. 
+gpa =3.883
+
+print (f'My gpa is {gpa} and it needs improvement to be 4.0 and hopefully I will get it next semester.')
+

f0d60cf0234dfceae23f950e88c2bde153b93330
Yonas G Bekele <ybekele25@uwcchina.org>
2025-09-29 23:54:11 +0800

    Input in Python

---
+++ input_python.py
@@ -0,0 +1,59 @@
+#This python file is all about inputs in python. 
+
+#input ("Enter your name to continue  ")
+
+name = input('What is your name? ')
+#print (f'Hello {name}!')
+
+#food = input ("What is your favorite food? ")
+#print (f'Awesome!')
+#freq= input ('How often do you eat it?')
+#print (f'You are true eatter, enjoy your {food}, {name}!')
+
+#The food request code
+
+#print (f'Hello {name}!')
+#age = input ('How old are you? ')
+#age =int(age)
+#age =age+1 
+#print (f'You are {age} years old.')
+
+#This is the age example
+#adjective =input('Enter an adjective: ')
+#noun = input ("Enter a noun: ")
+#adjective2 =input ('Enter adjective: ')
+#verb = input('Enter a verb: ')
+#adjective3 =input('Enter an adjective: ')
+
+#print(f'Today I went to a {adjective} zoo.')
+#print (f'In an exhibit, I saw {noun}')
+#print (f'{noun} was {adjective2} and {verb}ing')
+#print(f'I was {adjective3}')
+
+#This one was the adjective game example
+
+length = float(input('Enter the length of the rectangle: '))
+width =float(input ('Enter the width of the rectangle: '))
+area= 2*(length+width)
+area = float(area)
+
+print (f'The area of the rectangle is {area}cm^2"')
+
+#The shopping cart
+
+item =input ('What are you buying, today?: ')
+price=float(input("What is the price?: "))
+quantity = int(input('How many would you like to buy?: '))
+total =price*quantity 
+
+print (f'You have bought {quantity}*{price} $.')
+print(f'Your total is: ${round(total, 2)}')
+
+
+
+
+
+
+
+
+

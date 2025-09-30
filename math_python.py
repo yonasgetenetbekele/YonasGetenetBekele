7998ae0bc8a96d9fc07c35485f8d1215e6ee6508
Yonas G Bekele <ybekele25@uwcchina.org>
2025-09-30 23:10:56 +0800

    math in python

---
+++ math_python.py
@@ -0,0 +1,93 @@
+friends=0
+friends = friends +1
+
+print(friends)
+students =100
+students +=20
+print(students)
+
+cow =79
+cow = cow+20
+print (cow)
+
+#Subtraction 
+
+cart = 34
+cart -=4
+print (cart)
+egg = 20
+egg =egg-2
+print (egg)
+
+#Multiplication 
+
+days =7
+days *=52
+print(days)
+
+month = 12
+month = month*30
+print (month)
+
+#Division
+
+food = 24568
+food /= 2000
+print (food)
+
+cake=3000
+cake =int(cake/3)
+print(cake)
+
+#You can use arithmetic operators in python. By just assigning variables and performing the operation on the variables.
+
+#Here are built in functions:
+#We will explore some of the built in functions in Python
+#Round, absolute value, power, max, minimum...
+
+x=5.6899
+y=-6
+z=8
+
+result = round(x)
+absolute =abs(y)
+power =pow(z,3)
+maximum= max(x,y,z)
+minimum= min(x,y)
+
+print(result)
+print(absolute)
+print(power)
+print(maximum)
+print(minimum)
+
+#Next the math module
+
+import math 
+b=8.78
+c=100
+d=89.11
+
+print(math.pi)
+print(math.e)
+square =math.sqrt(b)
+mark=math.ceil(b)
+point =math.floor(d)
+
+print (square)
+print(mark)
+print (point)
+
+#Circumfrence of a circle
+
+radius =10
+
+circumference = 2*math.pi*radius
+print(circumference)
+
+#Area of a circle 
+
+area = pow(radius,2)*math.pi
+
+print(area)
+

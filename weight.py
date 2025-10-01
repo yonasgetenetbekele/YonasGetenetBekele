b8ede0ac9371d3b65aaaaef7216b91607dfabbf3
Yonas G Bekele <ybekele25@uwcchina.org>
2025-10-01 14:10:00 +0800

    python weight conversion exercise

---
+++ weight.py
@@ -0,0 +1,12 @@
+#Python weight convertor 
+weight = float(input('Please enter your weight: '))
+unit = input ('Kilogram or Pounds? (K or L)')
+if unit == "K":
+    weight = weight *2.205
+    unit =="Lbs."
+elif unit == "L":
+    weight = weight/2.205
+    unit == "Kgs."
+else:
+    print(f'{unit} was not valid. Please enter either L or K to continue.')
+print(f'Your weight is {weight} {unit}.')

0154a86df84eda018427f69d5834f6eebb952c55
Yonas G Bekele <ybekele25@uwcchina.org>
2025-10-04 23:48:24 +0800

    python compound interst program

---
+++ #This is the compound interst calcualtor.py
@@ -0,0 +1,21 @@
+#This is the compound interst calcualtor 
+
+principal = 0
+rate = 0
+time = 0
+
+while principal <=0:
+    principal = float(input('Enter the principal:'))
+    if principal <=0:
+        print ("Principal can't be less than zero.")
+
+while rate <=0:
+    rate = float(input('Enter the interst rate:'))
+    if rate <=0:
+        print ("Rate can't be less than zero.")
+        
+while time <=0:
+    time = int(input('Enter the time of the investment:'))
+    if time <=0:
+        print ("Time can't be less than zero.")
+        

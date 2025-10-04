15fac7a9a01f5b1761439aca50546da69f136494
Yonas G Bekele <ybekele25@uwcchina.org>
2025-10-04 16:34:26 +0800

    this is the email slicer exercise

--- email_slicer.py
+++ email_slicer.py
@@ -1,7 +1,7 @@
 #This is the email slicing exercise 
 email = input ("Enter your email: ")
-index = email.index("@")
-domain =email [index +1:] 
-username = email [:index]
+
+domain =email [email.index("@") +1:] 
+username = email [:email.index("@")]
 
 print (f'Your username is: {username}, and your domain is {domain}.')

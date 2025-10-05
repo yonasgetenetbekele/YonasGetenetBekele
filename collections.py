ab1cc46af0e69e4935840f1a4a7dff472de1280b
Yonas G Bekele <ybekele25@uwcchina.org>
2025-10-05 09:20:11 +0800

    this is a collections (list, set, and tuples)

--- collections.py
+++ collections.py
@@ -1,3 +1,40 @@
 #Collections in python List, set, and tuples
-fruit = ['apple', "orange", "banana", "cocunut"]
-print (fruit)
+fruits = ['apple', "orange", "banana", "cocunut"]
+print (dir(fruits))
+print (fruits[0:4:1])
+print(len(fruits))
+for fruit in fruits:
+    print (fruit)
+print ("pineapple" in fruits)
+
+fruits[1] = 'pineapple'
+for fruit in fruits:
+    print (fruit, "")
+
+fruits.append ("pineapple")
+fruits.remove ("apple")
+fruits.insert (0, "pineapple")
+fruits.sort ()
+fruits.reverse ()
+fruits.clear ()
+print(fruits.count ("banana"))
+print (fruits)
+
+books = {"apple", "orange", "banana", "cocunut"}
+print (len(books))
+books.add ("Holmes")
+books.clear
+books.remove ("Holmes")
+print (books)
+
+#Tuples 
+
+tuples = ("apples", "orange", "banana", "cocunut")
+print (tuples)
+print(len(tuples))
+print (tuples.index ("apples"))
+
+for book in books:
+    print (book)
+
+
